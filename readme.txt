cf8e5eb6d0e567d705bd1a333f3578bcbb1b4f07
jcorradino <jcorradino@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-28 15:43:27 +0000

    uploading searchable-categories plugin
    git-svn-id: https://plugins.svn.wordpress.org/searchable-categories/trunk@353240 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,21 @@
+=== Searchable Categories ===
+Contributors: Jason Corradino
+Donate link: http://example.com/
+Tags: admin, administration, meta, dashboard, post
+Requires at least: 3.1
+Tested up to: 3.1
+
+Searchable Categories allows a quick real-time search through categories while selecting in new post/edit post view.
+
+== Description ==
+
+This plugin will make it quick and easy for your writers to select categories related to what they are writing.  As they type in the search box, categories will be shown and hidden in real time, allowing them to easily select what is relevant to their content without having to scroll through possibly hundreds of categories.
+
+== Installation ==
+
+1. Upload plugin to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Changelog ==
+
+
---
+++ searchablecategories.php
@@ -0,0 +1,83 @@
+<?php
+/*
+Plugin Name: Back End Category Search
+Description: A back end search module which replaces the category selector in the Edit/Add post page to include searching categories
+Version: 0.1
+Author: Jason Corradino
+Author URI: http://imyourdeveloper.com/
+License: GPLv2 or later
+*/
+function searchable_category_meta_box( $post, $box ) {
+	$defaults = array('taxonomy' => 'category');
+	if ( !isset($box['args']) || !is_array($box['args']) )
+		$args = array();
+	else
+		$args = $box['args'];
+	extract( wp_parse_args($args, $defaults), EXTR_SKIP );
+	$tax = get_taxonomy($taxonomy);
+
+	?>
+		<script type="text/javascript">
+			jQuery(document).ready(function(){
+				jQuery('#catSearch').keyup(function(){
+					var val = jQuery('#catSearch').val();
+					jQuery('#categorychecklist li').hide();
+					jQuery('#categorychecklist').find(":contains('"+val+"')").show();
+				});
+			});
+		</script>
+	<div id="taxonomy-<?php echo $taxonomy; ?>" class="categorydiv">
+		Search: <input id="catSearch" type="text" value="" />
+		<ul id="<?php echo $taxonomy; ?>-tabs" class="category-tabs">
+			<li class="tabs"><a href="#<?php echo $taxonomy; ?>-all" tabindex="3"><?php echo $tax->labels->all_items; ?></a></li>
+			<li class="hide-if-no-js"><a href="#<?php echo $taxonomy; ?>-pop" tabindex="3"><?php _e( 'Most Used' ); ?></a></li>
+		</ul>
+		
+		<div id="<?php echo $taxonomy; ?>-pop" class="tabs-panel" style="display: none;">
+			<ul id="<?php echo $taxonomy; ?>checklist-pop" class="categorychecklist form-no-clear" >
+				<?php $popular_ids = wp_popular_terms_checklist($taxonomy); ?>
+			</ul>
+		</div>
+
+		<div id="<?php echo $taxonomy; ?>-all" class="tabs-panel">
+			<?php
+            $name = ( $taxonomy == 'category' ) ? 'post_category' : 'tax_input[' . $taxonomy . ']';
+            echo "<input type='hidden' name='{$name}[]' value='0' />"; // Allows for an empty term set to be sent. 0 is an invalid Term ID and will be ignored by empty() checks.
+            ?>
+			<ul id="<?php echo $taxonomy; ?>checklist" class="list:<?php echo $taxonomy?> categorychecklist form-no-clear">
+				<?php wp_terms_checklist($post->ID, array( 'taxonomy' => $taxonomy, 'popular_cats' => $popular_ids, 'checked_ontop' => false ) ) ?>
+			</ul>
+		</div>
+	<?php if ( current_user_can($tax->cap->edit_terms) ) : ?>
+			<div id="<?php echo $taxonomy; ?>-adder" class="wp-hidden-children">
+				<h4>
+					<a id="<?php echo $taxonomy; ?>-add-toggle" href="#<?php echo $taxonomy; ?>-add" class="hide-if-no-js" tabindex="3">
+						<?php
+							/* translators: %s: add new taxonomy label */
+							printf( __( '+ %s' ), $tax->labels->add_new_item );
+						?>
+					</a>
+				</h4>
+				<p id="<?php echo $taxonomy; ?>-add" class="category-add wp-hidden-child">
+					<label class="screen-reader-text" for="new<?php echo $taxonomy; ?>"><?php echo $tax->labels->add_new_item; ?></label>
+					<input type="text" name="new<?php echo $taxonomy; ?>" id="new<?php echo $taxonomy; ?>" class="form-required form-input-tip" value="<?php echo esc_attr( $tax->labels->new_item_name ); ?>" tabindex="3" aria-required="true"/>
+					<label class="screen-reader-text" for="new<?php echo $taxonomy; ?>_parent">
+						<?php echo $tax->labels->parent_item_colon; ?>
+					</label>
+					<?php wp_dropdown_categories( array( 'taxonomy' => $taxonomy, 'hide_empty' => 0, 'name' => 'new'.$taxonomy.'_parent', 'orderby' => 'name', 'hierarchical' => 1, 'show_option_none' => '&mdash; ' . $tax->labels->parent_item . ' &mdash;', 'tab_index' => 3 ) ); ?>
+					<input type="button" id="<?php echo $taxonomy; ?>-add-submit" class="add:<?php echo $taxonomy ?>checklist:<?php echo $taxonomy ?>-add button category-add-sumbit" value="<?php echo esc_attr( $tax->labels->add_new_item ); ?>" tabindex="3" />
+					<?php wp_nonce_field( 'add-'.$taxonomy, '_ajax_nonce-add-'.$taxonomy, false ); ?>
+					<span id="<?php echo $taxonomy; ?>-ajax-response"></span>
+				</p>
+			</div>
+		<?php endif; ?>
+	</div>
+	<?php
+}
+
+function update_collection_box() {
+	remove_meta_box( 'categorydiv', 'post', 'side');
+	add_meta_box('customcategorydiv', 'Categories', 'searchable_category_meta_box', 'post', 'side', 'core', array( 'taxonomy' => 'category' ));
+}
+
+add_filter('admin_menu', 'update_collection_box');
